adc5795dd4399b1a17dd23c2e8db2a998c9d307f
nomadhuns <kimalss22@gmail.com>
2023-07-04 12:18:52 +0900

    불필요한 테스트 어노테이션 제거

--- src/test/java/com/example/kakaoshop/repository/OptionJPARepositoryTest.java
+++ src/test/java/com/example/kakaoshop/repository/OptionJPARepositoryTest.java
@@ -1,18 +1,14 @@
 package com.example.kakaoshop.repository;
 
-import com.example.kakaoshop.product.Product;
 import com.example.kakaoshop.product.option.Option;
 import com.example.kakaoshop.product.option.OptionJPARepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class OptionJPARepositoryTest {
     @Autowired
